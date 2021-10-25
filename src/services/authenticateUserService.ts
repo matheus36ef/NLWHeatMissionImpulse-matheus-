03e9b3dad4eb3799a6e91e258ba6916336a8cea5
Matheus Franzoni <matheusfranzonef@gmail.com>
2021-10-25 11:02:47 -0300

    instalation axios / axiosTypes

--- src/services/authenticateUserService.ts
+++ src/services/authenticateUserService.ts
@@ -1,8 +1,18 @@
 
+/** Fluxo:
+ * Receber o codigo(string)
+ * Recuperar o access_token no github
+ * Verificar se o usuario existe no BD
+ *  - Se existir gerar um token 
+ *  - Se não existir criar no BD, gerar um token
+ * Retornar o token com as info do usuario logado
+ */
 
 
 class AuthenticateUserService {
-    async execute(code: string){
+    async execute(code: string){ // Recebendo o codigo(string).
+        //Recuperando o access_token. Para isso fazer um acesso ao github (intalar dependencia axios, + as tipagens do axios (@types/axios))
+
 
     }
 }
