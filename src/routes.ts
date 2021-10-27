edd4fe6a4cd9b263821631b39071a1456fa7d3a5
Matheus Franzoni <matheusfranzonef@gmail.com>
2021-10-27 19:07:10 -0300

    FinalizaçãoDaPrimeiraAula

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { authenticateUserController } from "./controllers/authenticateUserContro
 import { createdMessageController } from "./controllers/createdMessageController";
 import { ensureAuthenticate } from "../src/middleware/ensureAuthenticate";
 import { GetLast3MessagesControler } from "./controllers/GetLast3MessageControllers";
+import { ProfileUserController } from "./controllers/ProfileUserContoller";
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.post("/authenticate", new authenticateUserController().handle);
 router.post("/messages", ensureAuthenticate, new createdMessageController().handle);
 
 router.get("/messages/lest3", new GetLast3MessagesControler().handle);
+
+router.get("/profile", ensureAuthenticate, new ProfileUserController().handle);
 
 export { router }
